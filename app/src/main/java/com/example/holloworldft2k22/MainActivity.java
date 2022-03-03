6db6af31689b5a3fc9e72573e047d0b9b4d9ae3f
Edgmos <edgaras.mosiejus@gmail.com>
2022-03-03 23:49:11 +0200

    Added a main button and its operation

--- app/src/main/java/com/example/holloworldft2k22/MainActivity.java
+++ app/src/main/java/com/example/holloworldft2k22/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.holloworldft2k22;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView txtMain;
+    private boolean isHelloWorld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.txtMain = (TextView)findViewById(R.id.txtMain);
+        this.isHelloWorld = true;
+    }
+
+    public void onBtnMainClick(View view) {
+        if(this.isHelloWorld){
+            this.txtMain.setText("Well well well");
+            this.isHelloWorld = false;
+        }
+        else{
+            this.txtMain.setText(R.string.lable_name);
+            this.isHelloWorld = true;
+        }
     }
 }
